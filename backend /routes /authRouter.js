d89f76479402159936e22258d0380380d8486a66
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-08-15 00:15:37 +0530

    rest

---
+++ backend /routes /authRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { signUp, signIn, getUser, logout } = require('../controller/authController');
+const jwtAuth = require('../middleware/jwtAuth');
+const authRouter = express.Router();
+
+authRouter.post('/signup', signUp);
+authRouter.post('/signin', signIn);
+authRouter.get('/user', jwtAuth,getUser);
+authRouter.get('/logout',jwtAuth,logout)
+module.exports = authRouter;

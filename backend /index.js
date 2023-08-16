f70feb972a11ff9ac42e783b46d920d766802aad
Pranesh Deshmukh <praneshd2003@gmail.com>
2023-08-16 23:55:20 +0530

    Server Up

---
+++ backend /index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = require('./app.js');
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
